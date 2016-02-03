2df973809028c452c3993bf3db97b71333eb6914
uchennafokoye <okoyefaithful@gmail.com>
2016-02-03 13:51:30 -0600

    Updated UI-Design

--- UI-Design/app/src/main/java/com/uchennafokoye/uidesign/MainActivity.java
+++ UI-Design/app/src/main/java/com/uchennafokoye/uidesign/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_activity) {
             return true;
         }
 
