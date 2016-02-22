a0c09b4f297917a15b05e6082f55f6f2094b4f1b
Advait Patel <advaitpa93@gmail.com>
2016-02-22 02:23:00 -0600

    Adding resources for Reminders

---
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/reminders/CustomReminderActivity.java
@@ -0,0 +1,7 @@
+package edu.depaul.csc595.jarvis.reminders;
+
+/**
+ * Created by Advait on 22-02-2016.
+ */
+public class CustomReminderActivity {
+}
--- Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/reminders/ui/CreateEditActivity.java
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/reminders/ui/CreateEditActivity.java
@@ -1,7 +1,80 @@
 package edu.depaul.csc595.jarvis.reminders.ui;
 
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import edu.depaul.csc595.jarvis.R;
+
 /**
  * Created by Advait on 18-02-2016.
  */
-public class CreateEditActivity {
+public class CreateEditActivity extends AppCompatActivity
+{
+
+    @Bind(R.id.create_coordinator) CoordinatorLayout mCoordinatorLayout;
+    @Bind(R.id.notification_title) EditText mTitleEditText;
+    @Bind(R.id.notification_content) EditText mContentEditText;
+    @Bind(R.id.time)
+    TextView mTimeText;
+    @Bind(R.id.date) TextView mDateText;
+    @Bind(R.id.repeat_day) TextView mRepeatText;
+    @Bind(R.id.switch_toggle)
+    SwitchCompat mForeverSwitch;
+    @Bind(R.id.show_times_number) EditText mTimesEditText;
+    @Bind(R.id.forever_row)
+    TableRow mForeverRow;
+    @Bind(R.id.bottom_row) TableRow mBottomRow;
+    @Bind(R.id.bottom_view)
+    View mBottomView;
+    @Bind(R.id.show) TextView mShowText;
+    @Bind(R.id.times) TextView mTimesText;
+    @Bind(R.id.select_icon_text) TextView mIconText;
+    @Bind(R.id.select_colour_text) TextView mColourText;
+    @Bind(R.id.colour_icon)
+    ImageView mImageColourSelect;
+    @Bind(R.id.selected_icon) ImageView mImageIconSelect;
+    @Bind(R.id.error_time) ImageView mImageWarningTime;
+    @Bind(R.id.error_date) ImageView mImageWarningDate;
+    @Bind(R.id.error_show) ImageView mImageWarningShow;
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    private AlertDialog mIconSelectorDialog;
+    private AlertDialog mColourSelectorDialog;
+    private Calendar mCalendar;
+    private int mTimesShown;
+    private int mRepeatType;
+    private boolean[] mDaysOfWeek;
+    private String mIcon;
+    private String mColour;
+    private boolean newNotification;
+    private int mId;
+    private String[] mColourNames;
+    private String[] mColoursArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_custom_reminder_create);
+
+        ButterKnife.bind(this);
+
+
+
+
+    }
 }
