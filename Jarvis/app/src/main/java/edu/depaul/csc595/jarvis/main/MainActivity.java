43e40bb2102d4d7e7053331968adab6d6d48555d
uchennafokoye <okoyefaithful@gmail.com>
2016-02-23 12:59:37 -0600

    Removed floating action bar.

--- Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/main/MainActivity.java
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/main/MainActivity.java
@@ -2,7 +2,6 @@ package edu.depaul.csc595.jarvis.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
@@ -51,15 +50,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         //NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
