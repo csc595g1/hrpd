4a88b1aae48d1ad656984c6a099f19c61ab22417
Advait Patel <advaitpa93@gmail.com>
2016-02-24 02:09:51 -0600

    adding prevention home screen UI

--- Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/reminders/ReminderActivity.java
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/reminders/ReminderActivity.java
@@ -15,31 +15,6 @@ public class ReminderActivity extends AppCompatActivity
 {
     Button linkForCustom;
 
-    String[] country = new String[] {
-            "Advait",
-            "A",
-            "CSC",
-            "595",
-            "Android",
-            "ALlState",
-            "Jarivs",
-            "Capstone",
-            "IOT",
-            "HRPD"
-
-    };
-    public boolean[] status = {
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false,
-            false
-    };
 
 
     @Override
@@ -58,67 +33,5 @@ public class ReminderActivity extends AppCompatActivity
             }
         });
 
-        /*
-        if(savedInstanceState!=null){
-            status = savedInstanceState.getBooleanArray("status");
-        }
-        ListView lvCountries = (ListView) findViewById(R.id.lv_inventory);
-
-        OnItemClickListener itemClickListener = new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> lv, View item, int position, long id) {
-                // TODO Auto-generated method stub
-                ListView lview = (ListView) lv;
-
-                SimpleAdapter adapter = (SimpleAdapter) lview.getAdapter();
-
-                HashMap<String,Object> hm = (HashMap) adapter.getItem(position);
-                RelativeLayout rLayout = (RelativeLayout) item;
-                ToggleButton tgl = (ToggleButton) rLayout.getChildAt(1);
-                String strStatus = "";
-                if(tgl.isChecked()){
-                    tgl.setChecked(false);
-                    strStatus = "Off";
-                    status[position]=false;
-                }else{
-                    tgl.setChecked(true);
-                    strStatus = "On";
-                    status[position]=true;
-                }
-                Toast.makeText(getBaseContext(), (String) hm.get("txt") + " : " + strStatus, Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        lvCountries.setOnItemClickListener(itemClickListener);
-        List<HashMap<String,Object>> aList = new ArrayList<HashMap<String,Object>>();
-        for(int i=0;i<10;i++){
-            HashMap<String, Object> hm = new HashMap<String,Object>();
-            hm.put("txt", country[i]);
-            hm.put("stat",status[i]);
-            aList.add(hm);
-        }
-        String[] from = {"txt","stat" };
-        int[] to = { R.id.tv_item, R.id.tgl_status};
-
-        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), aList, R.layout.appliances_listview_format, from, to);
-
-        lvCountries.setAdapter(adapter);
-
-    }
-
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-    */
     }
 }
