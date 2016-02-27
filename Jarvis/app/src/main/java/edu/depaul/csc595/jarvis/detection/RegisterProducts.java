50609e275a7848de74aaf74e03bceb32fe4ba6a2
uchennafokoye <okoyefaithful@gmail.com>
2016-02-27 10:47:44 -0600

    Added Scanner ability

--- Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/detection/RegisterProducts.java
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/detection/RegisterProducts.java
@@ -3,11 +3,13 @@ package edu.depaul.csc595.jarvis.detection;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,9 +20,6 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.Bind;
 
@@ -42,7 +41,7 @@ public class RegisterProducts extends AppCompatActivity {
     @Bind(R.id.rb_sensors)
     RadioGroup rb_sensors;
 
-    @Bind(R.id.scan_button)
+    @Bind(R.id.scanner)
     Button scanBtn;
 
     @Bind(R.id.appliance_name)
@@ -51,6 +50,8 @@ public class RegisterProducts extends AppCompatActivity {
     private static String LOG_TAG = "RegisterProducts";
 
     String email;
+
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 
 
     private TextView formatTxt, contentTxt;
@@ -176,6 +177,86 @@ public class RegisterProducts extends AppCompatActivity {
             }
         });
         builder.show();
+    }
+
+    @OnClick(R.id.scanner)
+    public void scanBar(View v){
+
+        try {
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException e){
+            showDialog(RegisterProducts.this, "No Scanner Found",
+                    "Download a scanner code activity?", "Yes", "No").show();
+        }
+
+    }
+
+    public void scanQR(View v){
+
+        try {
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException e){
+            showDialog(RegisterProducts.this, "No Scanner Found",
+                    "Download a scanner code activity?", "Yes", "No").show();
+        }
+
+    }
+
+    private static AlertDialog showDialog(final AppCompatActivity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo){
+
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(buttonYes,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+//                                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                                Uri uri = Uri.parse("http://play.google.com/store/apps/details?id=" + "com.google.zxing.client.android");
+//                                Uri uri = Uri.parse("market://details?id=" + "com.google.zxing.client.android");
+
+                                Log.d(LOG_TAG, "URI: " + uri);
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                intent.setData(uri);
+                                try {
+                                    Log.d(LOG_TAG, "About to start activity for market");
+                                    act.startActivity(intent);
+                                } catch (ActivityNotFoundException e) {
+                                    Log.d(LOG_TAG, "Market: Zwing package not found");
+                                    e.printStackTrace();
+                                }
+                            }
+                        })
+                .setNegativeButton(buttonNo,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }
+
+                );
+
+        return downloadDialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        if (requestCode == 0){
+            if (resultCode == RESULT_OK){
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                et_barcode.setText(contents);
+                Log.d(LOG_TAG,
+                        "Content: " + contents + " Format:" + format
+                        );
+            }
+        }
     }
 
     @Override
