b5811e6cdf6c7d92edbff1d5e397bb9c7bcff30f
ed-miller1 <miller.edwardg@gmail.com>
2016-02-26 23:48:41 -0600

    adding shell classes for rewards balance

---
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/rewards/RewardBalanceFragment.java
@@ -0,0 +1,17 @@
+package edu.depaul.csc595.jarvis.rewards;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ed on 2/26/2016.
+ */
+public class RewardBalanceFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+}
---
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/rewards/adapters/RewardBalanceAdapter.java
@@ -0,0 +1,7 @@
+package edu.depaul.csc595.jarvis.rewards.adapters;
+
+/**
+ * Created by Ed on 2/26/2016.
+ */
+public class RewardBalanceAdapter {
+}
