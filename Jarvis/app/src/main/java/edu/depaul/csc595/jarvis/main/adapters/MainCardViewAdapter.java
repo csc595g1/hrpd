e3a295d011bbb7eae6581554694b1cc2d428e2ff
ed-miller1 <miller.edwardg@gmail.com>
2016-03-01 20:17:22 -0600

    added logic for card whether or not user is logged in

--- Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/main/adapters/MainCardViewAdapter.java
+++ Jarvis/app/src/main/java/edu/depaul/csc595/jarvis/main/adapters/MainCardViewAdapter.java
@@ -1,5 +1,6 @@
 package edu.depaul.csc595.jarvis.main.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,12 @@ import android.widget.TextView;
 import java.util.List;
 
 import edu.depaul.csc595.jarvis.R;
+import edu.depaul.csc595.jarvis.main.MainActivity;
 import edu.depaul.csc595.jarvis.main.card_view_model.CardViewModel;
+import edu.depaul.csc595.jarvis.profile.ProfileActivity;
 import edu.depaul.csc595.jarvis.profile.user.UserInfo;
 import edu.depaul.csc595.jarvis.rewards.HerokuAPI.GetTotalPointsAsyncTask;
+import edu.depaul.csc595.jarvis.rewards.RewardsActivity;
 
 /**
  * Created by Ed on 2/23/2016.
@@ -19,12 +23,14 @@ import edu.depaul.csc595.jarvis.rewards.HerokuAPI.GetTotalPointsAsyncTask;
 public class MainCardViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Object> cardList;
+    private MainActivity mainActivity;
 
     private static final int TYPE_REWARD = 0;
     private static final int TYPE_GENERIC = 1;
 
-    public MainCardViewAdapter(List<Object> cardList){
+    public MainCardViewAdapter(List<Object> cardList, MainActivity activity){
         this.cardList = cardList;
+        mainActivity = activity;
     }
 
     @Override
@@ -91,6 +97,25 @@ public class MainCardViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         if(UserInfo.getInstance().getIsLoggedIn() || UserInfo.getInstance().isGoogleLoggedIn()) {
             GetTotalPointsAsyncTask task = new GetTotalPointsAsyncTask();
             task.execute(holder);
+            holder.tv_redeem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mainActivity, RewardsActivity.class);
+                    //intent.
+                    mainActivity.startActivity(intent);
+                }
+            });
+        }
+        else{
+            holder.tv_content.setText("0");
+            holder.tv_redeem.setText("Log In or Sign Up to Get Rewards!");
+            holder.tv_redeem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mainActivity,ProfileActivity.class);
+                    mainActivity.startActivity(intent);
+                }
+            });
         }
     }
 
