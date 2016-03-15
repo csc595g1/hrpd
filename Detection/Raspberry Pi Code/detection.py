c99135c2c2a98cc69d917142e38459456df6e647
uchennafokoye <okoyefaithful@gmail.com>
2016-03-15 15:39:08 -0500

    Modified Detection code

--- Detection/Raspberry Pi Code/detection.py
+++ Detection/Raspberry Pi Code/detection.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-import serial, requests, time, json, io
+import serial, requests, time, json, io, threading
 port = '/dev/ttyACM1'
 port2 = '/dev/ttyACM0'
 ser = serial.Serial(port,  9600, timeout=0.50)
 ser2 = serial.Serial(port2, 9600, timeout=0.50)
 url = 'http://detectionservices.herokuapp.com'
 #url = 'http://localhost:3000'
+port1Notified = False
+port2Notified = False
 
 if ser.closed:
     ser.open()
@@ -26,28 +28,38 @@ def sendScript( line ):
         else:
             print(res.content)
             print(res.reason)
-    except (ValueError, AttributeError, io.BlockingIOError, serial.serialutil.SerialException)as e:
+    except (KeyError, ValueError, AttributeError, io.BlockingIOError, serial.serialutil.SerialException,requests.exceptions.ConnectionError )as e:
         print("Error: ", e)
         return False
     return True
 
+def setToFalseIn30 (variable):
+    print "About to set " + variable + "back to False"
+    globals()[variable] = False
+
+def printGlobals():
+    print "Port1Notified is: " + str(port1Notified)
+    print "Port2Notified is: " + str(port2Notified)
+
+def flushInput():
+    ser.flushInput()
+    ser2.flushInput()
+
 while 1:
-    print('We are currently outside')
     try:
         line = ser.readline().decode().strip('\r\n')
-        print("What serial 1 output looks like: " + line) 
+        print("serial 1: " + line) 
         line2 = ser2.readline().decode().strip('\r\n')
-        print("what serial 2 output looks like: " + line2)
-        success = False
-        success2 = False
-        if line != '':
-            success = sendScript(line)
-        if line2 != '':
-            success2 = sendScript(line2)
-        if success:
-            ser.flushInput()
-        if success2:
-            ser2.flushInput()
-# time.sleep(900) #if both success, sleep for 15 minutes
-    except (ValueError, io.BlockingIOError, serial.serialutil.SerialException)as e:
+        print("serial 2: " + line2)
+        if line != '' and not port1Notified:
+            global port1Notified
+            port1Notified = sendScript(line)
+            threading.Timer(30.0, setToFalseIn30, ["port1Notified"]).start()
+        if line2 != '' and not port2Notified :
+            global port2Notified
+            port2Notified = sendScript(line2)
+            threading.Timer(30.0, setToFalseIn30, ["port2Notified"]).start()
+        flushInput()
+        printGlobals()    
+    except (io.BlockingIOError, serial.serialutil.SerialException)as e:
         print("Readline temporarily unavailable. Trying again. Exception: ", e)
